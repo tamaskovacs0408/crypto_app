a17998f74ed7be7c861f0a17631c1a0a1e5a9d97
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-14 20:17:22 +0200

    Import components to Main Menu component

--- src/Components/MainMenu/MainMenu.jsx
+++ src/Components/MainMenu/MainMenu.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React from "react";
+import BTC from '../BTC/BTC';
+import ETH from '../ETH/ETH';
+import BNB from '../BNB/BNB';
+import ADA from '../ADA/ADA';
+import AVAX from '../AVAX/AVAX';
+import Doge from '../Doge/Doge';
+import SOL from '../SOL/SOL';
 
 export default function MainMenu() {
   return (
-    <div>MainMenu</div>
-  )
+    <div className="main_menu_container">
+      <BTC />
+      <ETH />
+      <BNB />
+      <ADA />
+      <AVAX />
+      <Doge />
+      <SOL />
+    </div>
+  );
 }
