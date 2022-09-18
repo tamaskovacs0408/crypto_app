a03bac1fdc01f55a3eb514c5a984edc7673b53af
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-18 12:22:47 +0200

    Update

---
+++ src/Components/Currency/Currency.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+//import "./style.css";
+import {
+  Accordion,
+  AccordionItemHeading,
+  AccordionItem,
+  AccordionItemPanel,
+  AccordionItemButton,
+} from "react-accessible-accordion";
+
+export default function Currency({ data }) {
+  let priceChange = data.quotes.USD.volume_24h_change_24h;
+
+  return (
+    <div className="accordion_container">
+      <Accordion allowZeroExpanded>
+        <AccordionItem>
+          <AccordionItemHeading>
+            <AccordionItemButton>
+              <div className="crypto_container">
+                <div className="crypto_name">
+                  <span>{data.rank}</span>
+                  <h2>{data.name}</h2>
+                </div>
+                <div className="crypto_price">
+                  <p>{parseFloat(data.quotes.USD.price).toFixed(2)} $</p>
+                  <p style={{ color: priceChange < 0 ? "red" : "#169c11" }}>
+                    {parseFloat(priceChange).toFixed(1)} %
+                  </p>
+                </div>
+              </div>
+            </AccordionItemButton>
+          </AccordionItemHeading>
+          <AccordionItemPanel>
+            <div className="more_information_container">
+              <div className="volume">
+                <p>Market cap: {data.max_supply}</p>
+              </div>
+            </div>
+          </AccordionItemPanel>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
+}
