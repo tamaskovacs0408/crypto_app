8df9b306469f4235b54693da71fc5b1c938e75cc
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-18 09:59:08 +0200

    Change BTC to accordion (basic)

--- src/Components/BTC/BTC.jsx
+++ src/Components/BTC/BTC.jsx
@@ -1,19 +1,37 @@
 import React from "react";
-import './style.css'
+import "./style.css";
+import {
+  Accordion,
+  AccordionItemHeading,
+  AccordionItem,
+  AccordionItemPanel,
+  AccordionItemButton,
+} from "react-accessible-accordion";
 
-export default function BTC({data}) {
+export default function BTC({ data }) {
   let priceChange = data.priceChangePercent;
 
   return (
-    <div className="crypto_container">
-      <div className="crypto_name">
-        <h2 className="BTC">Bitcoin</h2>
-        {/* <button>Current price</button> */}
-      </div>
-      <div className="crypto_price">
-        <p>{parseFloat(data.lastPrice).toFixed(2)} $</p>
-        <p style={{color: (priceChange < 0) ? 'red' : '#169c11'}}>{parseFloat(priceChange).toFixed(1)} %</p>
-      </div>
+    <div className="accordion_container">
+      <Accordion allowZeroExpanded>
+        <AccordionItem>
+          <AccordionItemHeading>
+            <AccordionItemButton>
+              <div className="crypto_container">
+                <div className="crypto_name">
+                  <h2 className="BTC">Bitcoin</h2>
+                </div>
+                <div className="crypto_price">
+                  <p>{parseFloat(data.lastPrice).toFixed(2)} $</p>
+                  <p style={{ color: priceChange < 0 ? "red" : "#169c11" }}>
+                    {parseFloat(priceChange).toFixed(1)} %
+                  </p>
+                </div>
+              </div>
+            </AccordionItemButton>
+          </AccordionItemHeading>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 }
