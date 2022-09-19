8ccb2993e1b95706249eb84fd590aea7b55cffc4
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-19 12:57:11 +0200

    Create LogIn component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.scss';
 import SignUp from './Components/SignUp/SignUp';
+import LogIn from './Components/LogIn/LogIn';
 import MainMenu from './Components/MainMenu/MainMenu';
 
 function App() {
   return (
     <div className="App">
       <SignUp />
+      <LogIn />
       {/* <MainMenu /> */}
     </div>
   );
