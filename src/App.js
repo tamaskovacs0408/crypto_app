2e3aca7e0a591948f729bd1826c80e86be52a60d
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-19 10:35:44 +0200

    Create signup component, temp disable main menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.scss';
+import SignUp from './Components/SignUp/SignUp';
 import MainMenu from './Components/MainMenu/MainMenu';
 
 function App() {
   return (
     <div className="App">
-      <MainMenu />
+      {/* <MainMenu /> */}
     </div>
   );
 }
