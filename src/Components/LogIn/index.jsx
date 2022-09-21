e92ef94c89c5b0c255ab1442d81952624aca30f7
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-21 16:47:45 +0200

    Login component update

--- src/Components/LogIn/index.jsx
+++ src/Components/LogIn/index.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from "react";
 import "./index.scss";
 
 export default function LogIn() {
+  const [loginemail, setLoginemail] = useState("");
+  const [loginpassword, setLoginpassword] = useState("");
+
   return (
-    <div className='login_container'>
+    <div className="login_container">
       <from>
         <h2>Login</h2>
         <label htmlFor="login_email">
-          <input type="text" />
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setLoginemail(e.target.value)}
+            required
+          />
         </label>
         <label htmlFor="login_password">
-          <input type="text" />
+          <input
+            type="password"
+            placeholder="password"
+            onChange={(e) => setLoginpassword(e.target.value)}
+            required
+          />
         </label>
       </from>
     </div>
-  )
+  );
 }
