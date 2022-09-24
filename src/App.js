a6d7dc1c2bf2c9fec303a785242a3d2a965a8149
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-24 10:32:41 +0200

    Update Routes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 import SignUp from "./Components/SignUp";
 import LogIn from "./Components/LogIn";
-import MainMenu from "./Components/MainMenu";
+
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path="/" element={<LogIn/>}/>
+        <Route path="/signup" element={<SignUp />}/>
+      </Routes>
     </div>
   );
 }
