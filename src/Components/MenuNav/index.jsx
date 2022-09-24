d9733995bacbaa17fda7ecf5533a6f71337143e8
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-24 12:15:57 +0200

    Update

---
+++ src/Components/MenuNav/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./index.scss"
+
+export default function MenuNav() {
+  return (
+    <div className="navbar">
+        <Link to="/">Log out</Link>
+      </div>
+  )
+}
