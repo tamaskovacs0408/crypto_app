c0c3bd50af77339222a63af7f3eed22f4d4a82d1
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-25 21:34:18 +0200

    Update

--- src/Components/MainMenu/index.jsx
+++ src/Components/MainMenu/index.jsx
@@ -3,16 +3,9 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import uuid from "react-uuid";
 import { COIN_API } from "../API";
-// import { COIN_DATA_API } from "../API";
 import Currency from "../Currency";
 import MenuNav from "../MenuNav";
-// import BTC from "../BTC/BTC";
-// import ETH from "../ETH/ETH";
-// import BNB from "../BNB/BNB";
-// import ADA from "../ADA/ADA";
-// import AVAX from "../AVAX/AVAX";
-// import Doge from "../Doge/Doge";
-// import SOL from "../SOL/SOL";
+
 
 export default function MainMenu() {
   const [coinData, setCoinData] = useState([]);
