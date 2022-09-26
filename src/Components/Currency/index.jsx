9e9fa048be5478da978b5950e136d5291516a58d
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-26 12:06:59 +0200

    update

--- src/Components/Currency/index.jsx
+++ src/Components/Currency/index.jsx
@@ -12,14 +12,14 @@ import {
 export default function Currency({ id, data }) {
   let priceChange = data.quotes.USD.percent_change_24h;
   let athDate = data.quotes.USD.ath_date.split("T")[0];
-  const [active, setActive] = useState(() => {
-    let fav = localStorage.getItem("active");
-    if (fav) {
-      return (fav = JSON.parse(localStorage.getItem("fav")));
-    } else {
-      return [];
-    }
-  });
+  const [active, setActive] = useState(false
+    // let fav = localStorage.getItem("active");
+    // if (fav) {
+    //   return (fav = JSON.parse(localStorage.getItem(fav)));
+    // } else {
+    //   return [];
+    // }
+  );
 
   const handleChangeActive = () => {
     setActive((previousHeart) => {
@@ -28,55 +28,52 @@ export default function Currency({ id, data }) {
   };
 
   useEffect(() => {
-    localStorage.setItem("active", JSON.stringify(active))
-  }, [active])
+    localStorage.setItem("active", JSON.stringify(active, id));
+  }, [active, id]);
 
   return (
     <div className="accordion_container">
-      <div className="fav_container">
-        {active ? (
-          <IoHeart
-            className="active"
-            onClick={() => handleChangeActive()}
-          />
-        ) : (
-          <IoHeartOutline
-            className="inactive"
-            onClick={() => handleChangeActive()}
-          />
-        )}
-      </div>
-      <Accordion allowZeroExpanded>
-        <AccordionItem>
-          <AccordionItemHeading>
-            <AccordionItemButton>
-              <div className="crypto_container">
-                <div className="crypto_name">
-                  <p>{data.rank}</p>
-                  <h2>{data.name}</h2>
-                  <p>{data.symbol}</p>
+          <Accordion allowZeroExpanded>
+            <AccordionItem>
+              <AccordionItemHeading>
+                <AccordionItemButton>
+                  <div className="crypto_container">
+                  <div className="fav_container">
+          {active ? (
+            <IoHeart className="active" onClick={() => handleChangeActive()} />
+          ) : (
+            <IoHeartOutline
+              className="inactive"
+              onClick={() => handleChangeActive()}
+            />
+          )}
+        </div>
+                    <div className="crypto_name">
+                      <p>{data.rank}</p>
+                      <h2>{data.name}</h2>
+                      <p>{data.symbol}</p>
+                    </div>
+                    <div className="crypto_price">
+                      <p>{parseFloat(data.quotes.USD.price).toFixed(2)} $</p>
+                      <p style={{ color: priceChange < 0 ? "red" : "#169c11" }}>
+                        {parseFloat(priceChange).toFixed(1)} %
+                      </p>
+                    </div>
+                  </div>
+                </AccordionItemButton>
+              </AccordionItemHeading>
+              <AccordionItemPanel>
+                <div className="data_container">
+                  <div className="datas">
+                    <p>Market cap: {data.quotes.USD.market_cap}</p>
+                    <p>Circulating supply: {data.circulating_supply}</p>
+                    <p>All Time High price: {data.quotes.USD.ath_price}</p>
+                    <p>All Time High price date: {athDate}</p>
+                  </div>
                 </div>
-                <div className="crypto_price">
-                  <p>{parseFloat(data.quotes.USD.price).toFixed(2)} $</p>
-                  <p style={{ color: priceChange < 0 ? "red" : "#169c11" }}>
-                    {parseFloat(priceChange).toFixed(1)} %
-                  </p>
-                </div>
-              </div>
-            </AccordionItemButton>
-          </AccordionItemHeading>
-          <AccordionItemPanel>
-            <div className="data_container">
-              <div className="datas">
-                <p>Market cap: {data.quotes.USD.market_cap}</p>
-                <p>Circulating supply: {data.circulating_supply}</p>
-                <p>All Time High price: {data.quotes.USD.ath_price}</p>
-                <p>All Time High price date: {athDate}</p>
-              </div>
-            </div>
-          </AccordionItemPanel>
-        </AccordionItem>
-      </Accordion>
+              </AccordionItemPanel>
+            </AccordionItem>
+          </Accordion>
     </div>
   );
 }
