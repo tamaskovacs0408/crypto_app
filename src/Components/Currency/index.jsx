6ee4804d7473111163a570dbc43ad414f05dedcb
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-29 07:23:53 +0200

    Update

--- src/Components/Currency/index.jsx
+++ src/Components/Currency/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { IoHeart, IoHeartOutline } from "react-icons/io5";
+// import { IoHeart, IoHeartOutline } from "react-icons/io5";
 import "./index.scss";
 import {
   Accordion,
@@ -12,68 +12,55 @@ import {
 export default function Currency({ id, data }) {
   let priceChange = data.quotes.USD.percent_change_24h;
   let athDate = data.quotes.USD.ath_date.split("T")[0];
-  const [active, setActive] = useState(false
-    // let fav = localStorage.getItem("active");
-    // if (fav) {
-    //   return (fav = JSON.parse(localStorage.getItem(fav)));
-    // } else {
-    //   return [];
-    // }
-  );
+  athDate = athDate.replace(/-/g, '.');
 
-  const handleChangeActive = () => {
-    setActive((previousHeart) => {
-      return !previousHeart;
-    });
-  };
-
-  useEffect(() => {
-    localStorage.setItem("active", JSON.stringify(active, id));
-  }, [active, id]);
 
   return (
     <div className="accordion_container">
-          <Accordion allowZeroExpanded>
-            <AccordionItem>
-              <AccordionItemHeading>
-                <AccordionItemButton>
-                  <div className="crypto_container">
-                  <div className="fav_container">
-          {active ? (
-            <IoHeart className="active" onClick={() => handleChangeActive()} />
-          ) : (
-            <IoHeartOutline
-              className="inactive"
-              onClick={() => handleChangeActive()}
-            />
-          )}
-        </div>
-                    <div className="crypto_name">
-                      <p>{data.rank}</p>
-                      <h2>{data.name}</h2>
-                      <p>{data.symbol}</p>
-                    </div>
-                    <div className="crypto_price">
-                      <p>{parseFloat(data.quotes.USD.price).toFixed(2)} $</p>
-                      <p style={{ color: priceChange < 0 ? "red" : "#169c11" }}>
-                        {parseFloat(priceChange).toFixed(1)} %
-                      </p>
-                    </div>
-                  </div>
-                </AccordionItemButton>
-              </AccordionItemHeading>
-              <AccordionItemPanel>
-                <div className="data_container">
-                  <div className="datas">
-                    <p>Market cap: {data.quotes.USD.market_cap}</p>
-                    <p>Circulating supply: {data.circulating_supply}</p>
-                    <p>All Time High price: {data.quotes.USD.ath_price}</p>
-                    <p>All Time High price date: {athDate}</p>
-                  </div>
+      <Accordion allowZeroExpanded>
+        <AccordionItem>
+          <AccordionItemHeading>
+            <AccordionItemButton>
+              <div className="crypto_container">
+                <div className="crypto_name">
+                  <p>{data.rank}</p>
+                  <h2>{data.name}</h2>
+                  <p>{data.symbol}</p>
                 </div>
-              </AccordionItemPanel>
-            </AccordionItem>
-          </Accordion>
+                <div className="crypto_price">
+                  <p>{parseFloat(data.quotes.USD.price).toFixed(2)} $</p>
+                  <p style={{ color: priceChange < 0 ? "red" : "#169c11" }}>
+                    {parseFloat(priceChange).toFixed(1)} %
+                  </p>
+                </div>
+              </div>
+            </AccordionItemButton>
+          </AccordionItemHeading>
+          <AccordionItemPanel>
+            <div className="data_container">
+              <div className="data_inner_container">
+                <div className="datas">
+                  <h3>Market cap:</h3>
+                  <p>{data.quotes.USD.market_cap}</p>
+                </div>
+                <div className="datas">
+                  <h3>Circulating supply:</h3>
+                  <p>{data.circulating_supply}</p>
+                </div>
+                <div className="datas">
+                  {" "}
+                  <h3>All Time High price: </h3>
+                  <p>{parseFloat(data.quotes.USD.ath_price).toFixed(2)}$</p>
+                </div>
+                <div className="datas">
+                  <h3>All Time High price date: </h3>
+                  <p>{athDate}</p>
+                </div>
+              </div>
+            </div>
+          </AccordionItemPanel>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 }
