9ae1eeac9ba46106497f1f5c8413ba6c29d3369d
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-29 08:25:26 +0200

    Set up API

--- src/Components/GlobalData/index.jsx
+++ src/Components/GlobalData/index.jsx
@@ -1,13 +1,16 @@
-import React, {useState, useEffect} from 'react';
-import './index.scss';
-import { GLOBAL_DATA_API } from '../API';
+import React, { useState, useEffect } from "react";
+import "./index.scss";
+import { GLOBAL_DATA_API } from "../API";
 
 export default function GlobalData() {
   const [globalData, setGlobalData] = useState([]);
 
+  useEffect(() => {
+    fetch(GLOBAL_DATA_API)
+      .then((response) => response.json())
+      .then((data) => setGlobalData(data));
+  }, []);
   
 
-  return (
-    <div>globaldata</div>
-  )
+  return <div>globaldata</div>;
 }
