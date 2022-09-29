5340c1fd8d2387e4205ccd598510c97381440837
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-29 07:44:09 +0200

    Create API for global data

--- src/Components/API.jsx
+++ src/Components/API.jsx
@@ -1,2 +1,2 @@
-// export const COIN_API = 'https://api.binance.com/api/v3/ticker/24hr';
 export const COIN_API = 'https://api.coinpaprika.com/v1/tickers';
+export const GLOBAL_DATA_API = 'https://api.coinpaprika.com/v1/global';
--- src/Components/GlobalData/index.jsx
+++ src/Components/GlobalData/index.jsx
@@ -1,8 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import './index.scss';
+import { GLOBAL_DATA_API } from '../API';
 
-export default function index() {
+export default function GlobalData() {
+  const [globalData, setGlobalData] = useState([]);
+
+  
+
   return (
-    <div>index</div>
+    <div>globaldata</div>
   )
 }
