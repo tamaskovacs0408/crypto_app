a9f332c8a9fe958b0f29f0c97de402ae91bbbf85
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-29 08:41:33 +0200

    Update API

--- src/Components/GlobalData/index.jsx
+++ src/Components/GlobalData/index.jsx
@@ -3,14 +3,16 @@ import "./index.scss";
 import { GLOBAL_DATA_API } from "../API";
 
 export default function GlobalData() {
-  const [globalData, setGlobalData] = useState([]);
+  const [gData, setgData] = useState([]);
 
   useEffect(() => {
     fetch(GLOBAL_DATA_API)
       .then((response) => response.json())
-      .then((data) => setGlobalData(data));
+      .then((data) => setgData(data))
+      .catch((err) => console.log(err));
   }, []);
-  
 
-  return <div>globaldata</div>;
+  return <div>
+    <p>{gData.market_cap_usd}</p>
+  </div>;
 }
