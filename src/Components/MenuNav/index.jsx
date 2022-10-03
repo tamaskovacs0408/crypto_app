8066cc760aa60c196af928ce365118ea8684e248
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-10-03 16:23:00 +0200

    Create hamburger menu

--- src/Components/MenuNav/index.jsx
+++ src/Components/MenuNav/index.jsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import "./index.scss";
 
 export default function MenuNav() {
+  const [showNavbar, setshowNavbar] = useState(false);
+
   return (
     <div className="navbar">
-      <Link className="nav_link" to="/menu">
-        Currencies
-      </Link>
-      <Link className="nav_link" to="/global">
-        Global Data
-      </Link>
-      <Link className="nav_link" to="/">
-        Log out
-      </Link>
+      <h2 className="logo">Crypt app</h2>
+      <nav>
+        <Link className="nav_link" to="/menu">
+          Currencies
+        </Link>
+        <Link className="nav_link" to="/global">
+          Global Data
+        </Link>
+        <Link className="nav_link" to="/">
+          Log out
+        </Link>
+      </nav>
+      <FontAwesomeIcon className="hamburger_icon" icon={faBars}/>
     </div>
   );
 }
