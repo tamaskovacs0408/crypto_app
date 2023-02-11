d641fe2fcbfa0504ac65bf45b3b75e3586b60ef7
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-02-11 09:37:47 +0100

    Change data fetching for custom hook

--- src/Components/MainMenu/index.jsx
+++ src/Components/MainMenu/index.jsx
@@ -1,21 +1,12 @@
 import "./index.scss";
-import React, { useState, useEffect } from "react";
-import axios from "axios";
 import uuid from "react-uuid";
-import { COIN_API } from "../API";
 import Currency from "../Currency";
 import MenuNav from "../MenuNav";
+import useCoinData from "../../Hooks/useCoinData";
 
 
 export default function MainMenu() {
-  const [coinData, setCoinData] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get(`${COIN_API}`)
-      .then((res) => setCoinData(res.data))
-      .catch((err) => console.log(err));
-  }, []);
+  const coinData = useCoinData();
 
   return (
     <div>
